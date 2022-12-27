ed1ad5c54ddcbf57bcd4b7f437541b4c911b7efc
Stephen Cremin <stephen@iffies.com>
2022-12-28 02:27:19 +0800

    Remove GitHub actions

--- mix.exs
+++ mix.exs
@@ -62,7 +62,6 @@ defmodule CodeHygiene.MixProject do
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:excoveralls, "~> 0.15", only: [:dev, :test], runtime: false},
-      {:git_hooks, "~> 0.7", only: [:test, :dev], runtime: false},
       {:phoenix_live_dashboard, "~> 0.7.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
